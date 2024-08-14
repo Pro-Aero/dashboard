b17d7c13cbdcf613aaf88d41b7fc9bb039050c14
PedrooMota <pedrohmota2005@gmail.com>
2024-08-14 13:35:57 -0300

    try catch auth

--- src/app/auth/sign-in/action.ts
+++ src/app/auth/sign-in/action.ts
@@ -2,5 +2,9 @@
 
 import { signIn } from "@/auth";
 export async function SignInAction() {
-  await signIn("azure-ad", { prompt: "login" });
+  try {
+    await signIn("azure-ad", { prompt: "login" });
+  } catch (err) {
+    console.log("erro de servidor?");
+  }
 }
